f9816b078dc8dee6ebacf47e10c9ef317a0de1ed
duncanrhamill <duncanrhamill@googlemail.com>
2016-04-15 14:54:01 +0100

    Added sum, sub and help start on lc

--- commands.js
+++ commands.js
@@ -2,13 +2,67 @@ var commands = [
             {
                 command: 'clear',
                 func: function() {
-                    lines = [];
-                }
+                    setLines([]);
+                },
+				help: 'Clears the screen'
             },
             {
                 command: 'echo',
-                func: function(text) {
+                func: function(args) {
+					var text = args.join(' ');
                     writeLine(text);
-                }
-            }
+                },
+				help: 'Prints the given text to the sceen'
+            },
+			{
+				command: 'sum',
+				func: function(args) {
+					var x;
+					var s = 0;
+					for (x of args) {
+						s += parseFloat(x);
+					}
+					
+					writeLine(s);
+				},
+				help: 'Adds all given numbers'
+			},
+			{
+				command: 'sub',
+				func: function(args) {
+					var x;
+					var s = 0;
+					for (x of args) {
+						s -= parseFloat(x);
+					}
+					
+					writeLine(s);
+				},
+				help: 'Subtracts all given numbers'
+			},
+			{
+				command: 'help',
+				func: function(args) {
+					var com;
+					var comFound = false;
+					for (com of commands) {
+						if (com.command == args[0]) {
+							writeLine(com.help);
+							comFound = true;
+						}
+					}
+					
+					if(!comFound) {
+						writeLine('No command found by that name');
+					}
+				},
+				help: 'Displays help text for a given command'
+			},
+			{
+				command: 'lc',
+				func: function(args) {
+					
+				},
+				help: 'unknown'
+			}
         ];
